1dc7ed3046a2321b3e639b2cdab6c3316a0b7213
Jeongho Kim <jeongho@charlie.kim>
2024-09-12 11:42:30 +0900

    Remove concurrency logic due to failure deleting

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import os
 from datetime import date, timedelta
 from io import BytesIO
-from multiprocessing.pool import ThreadPool
+from zoneinfo import ZoneInfo
 
 import piexif
 from PIL import Image
@@ -11,14 +11,15 @@ from pyicloud.services.photos import PhotoAsset
 
 
 def download_and_delete_photo(asset: PhotoAsset):
-    created_date_for_filename = asset.created.strftime("%Y%m%d_%H%M%S")
+    created_local_time = asset.created.replace(tzinfo=ZoneInfo('UTC')).astimezone(ZoneInfo('Asia/Seoul'))
+    created_date_for_filename = created_local_time.strftime("%Y%m%d_%H%M%S")
     name, extension = asset.filename.split(".")
     filename = '{}_{}.{}'.format(name, created_date_for_filename, extension)
 
     print('Download {}'.format(filename))
     download = asset.download(version='original')
     image = Image.open(BytesIO(download.content))
-    created_datetime = asset.created.strftime('%Y:%m:%d %H:%M:%S')
+    created_datetime = created_local_time.strftime('%Y:%m:%d %H:%M:%S')
 
     exif_dict = piexif.load(image.info['exif']) if 'exif' in image.info else {'0th': {}, 'Exif': {}, 'GPS': {},
                                                                               '1st': {}}
@@ -34,13 +35,11 @@ def download_and_delete_photo(asset: PhotoAsset):
 
 username = os.environ.get('PYICLOUD_USERNAME')
 download_path = os.environ.get('DOWNLOAD_PATH', 'downloads')
-number_of_thread_pool = os.environ.get('NUMBER_OF_THREADS_POOL', 4)
 api = PyiCloudService(username)
 
 if not os.path.exists(download_path):
     os.makedirs(download_path)
 
-candidates = []
 last_month = date.today() - timedelta(days=30)
 screenshots_iterator = iter(api.photos.albums['Screenshots'])
 while True:
@@ -49,13 +48,9 @@ while True:
         break
 
     if photo.created.date() > last_month:
-       break
+        break
 
     if photo.is_favorite:
         continue
 
-    candidates.append(photo)
-
-pool = ThreadPool(processes=number_of_thread_pool)
-pool.map(download_and_delete_photo, candidates)
-pool.close()
+    download_and_delete_photo(photo)
